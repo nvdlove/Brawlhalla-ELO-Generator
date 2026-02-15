a06b817e0b46e710e14f08ea01ca41e99b89509a
nvdlove <reallyundefine@gmail.com>
2026-02-15 16:02:42 -0500

    Update Form1.Designer.cs

--- BrawlhallaAutomation/Form1.Designer.cs
+++ BrawlhallaAutomation/Form1.Designer.cs
@@ -2,8 +2,15 @@ namespace BrawlhallaAutomation
 {
     partial class Form1
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -13,12 +20,34 @@ namespace BrawlhallaAutomation
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 500);
-            this.Text = "Form1";
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.titleBar = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            // ... declare all controls
+
+            this.SuspendLayout();
+
+            // Initialize your controls here
+            this.txtLog.Location = new System.Drawing.Point(0, 30);
+            this.txtLog.Size = new System.Drawing.Size(760, 445);
+            // ... set all properties
+
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.titleBar);
+            // ... add all controls
+
+            this.ResumeLayout(false);
         }
+
+        #endregion
     }
 }
